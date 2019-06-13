02c645150cf174b1662f1b4e8fd6b96d73dff5a2
Rhys Grabany <r.grabany01@qub.ac.uk>
2019-06-13 16:40:50 +0100

    Added functionality to the file input command

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ def decrypt():
 def encrypt():
     pass
 
-def readFile():
-    pass
+def readFile(args):
+    file_words = []
+
+    with args.input as fileI:
+        for line in fileI:
+            for word in line.split():
+                file_words.append(word)
+
+    return file_words
+
 
 def writeFile():
     pass
@@ -19,15 +27,15 @@ def writeFile():
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--decrypt", default=False, required=False, action="store_true", help="Decypted an input file")
 ap.add_argument("-e", "--encrypt", default=False, required=False, action="store_true", help="Encrypted an input file")
-ap.add_argument("-i", "--input", default=False, required=False, type=argparse.FileType('r'), help="Input a file to be encrypted or decrypted")
+ap.add_argument("-i", "--input", default=False, required=False, type=argparse.FileType('r'), dest="input", help="Input a file to be encrypted or decrypted")
 ap.add_argument("-s", "--step", default=False, required=False, type=int, help="How many steps to the encryption desired")
 
-arguments = vars(ap.parse_args())
+arguments = ap.parse_args()
 
-decrypt = arguments["decrypt"]
-encrypt = arguments["encrypt"]
-fileI = arguments["input"] 
-step = arguments["step"]
+decrypt = arguments.decrypt
+encrypt = arguments.encrypt
+fileI = arguments.input
+step = arguments.step
 
 stepCheck = False
 if isinstance(step, int):
@@ -40,6 +48,7 @@ if (decrypt and stepCheck):
     ap.error("Program automatically decrypts, no need for step")
 
 
+readFile(arguments)
 
 
 
