c50c13a59c8ecfd6e69d994599ec055679df75bf
Rhys Grabany <r.grabany01@qub.ac.uk>
2019-06-13 17:28:32 +0100

    Added file output functionality

--- main.py
+++ main.py
@@ -19,36 +19,49 @@ def readFile(args):
     return file_words
 
 
-def writeFile():
-    pass
+def writeFile(args, out):
+    with args.output as fileO:
+        for word in out:
+            fileO.write(word + " ")
+            
 
 
-    
+
+# Arguments used in this program for input, output, steps, encrypt, and decrypt
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--decrypt", default=False, required=False, action="store_true", help="Decypted an input file")
 ap.add_argument("-e", "--encrypt", default=False, required=False, action="store_true", help="Encrypted an input file")
 ap.add_argument("-i", "--input", default=False, required=False, type=argparse.FileType('r'), dest="input", help="Input a file to be encrypted or decrypted")
+ap.add_argument("-o", "--output", default="out.txt", required=False, type=argparse.FileType('w'), dest="output", help="File output, default is: out.txt")
 ap.add_argument("-s", "--step", default=False, required=False, type=int, help="How many steps to the encryption desired")
 
 arguments = ap.parse_args()
 
+# Load some arguments into variables that need to be used now
+# input, and output can be left until later
 decrypt = arguments.decrypt
 encrypt = arguments.encrypt
-fileI = arguments.input
 step = arguments.step
 
 stepCheck = False
 if isinstance(step, int):
     stepCheck = True
 
-
+# check if encrypt and decrypt are used together
 if (decrypt and encrypt):
     ap.error("Only allowed either encrypt or decrypt one at a time")
+# 
 if (decrypt and stepCheck):
     ap.error("Program automatically decrypts, no need for step")
 
+print(arguments.input, arguments.output)
 
-readFile(arguments)
+input_words = readFile(arguments)
+
+print(input_words)
+
+writeFile(arguments, input_words)
+
 
 
 
